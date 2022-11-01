f6fba3642f09fcbe6ddd5dc248255fa6cf446ec6
mdrgoncalves <mdr.rgon@gmail.com>
2022-11-01 13:58:31 -0300

    Parado no estudo de States

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import style from './Botao.module.scss';
+
+interface BotaoProps {
+    children: React.ReactNode;
+}
+
+class Botao extends React.Component<BotaoProps> {
+    render() {
+        return (
+            <button className={style.botao}>
+                {this.props.children}
+            </button>
+        )
+    }
+}
+
+export default Botao;
